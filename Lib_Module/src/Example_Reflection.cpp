d8b2810d44834704fccd56dca8c042ec3a2141af
GetBoros <getboros@gmail.com>
2026-07-05 21:58:36 +0300

    doc: example with remove cvref type

--- Lib_Module/src/Example_Reflection.cpp
+++ Lib_Module/src/Example_Reflection.cpp
@@ -9,22 +9,19 @@ module Example_Reflection;
 
 
 //------------------------------------------------------------------------------------------------------------
-struct SObject
+class AObject
 {
-    int Value;
+    int Value_00;
+    int Value_01;
 };
 //------------------------------------------------------------------------------------------------------------
 template<typename type_name> void TExample_Reflection(const type_name &object, int value)
 {
-    constexpr std::meta::info object_info = ^^object;
+    constexpr std::meta::info object_info = ^^std::remove_cvref_t<type_name>;
     constexpr std::meta::access_context access_ctx = std::meta::access_context::unchecked();
 
     static constexpr auto fields = std::define_static_array(std::meta::nonstatic_data_members_of(object_info, access_ctx) );
-    // static constexpr auto fields = std::define_static_array(std::meta::nonstatic_data_members_of(meta_info, access_ctx) );
-
-    // std::println("Hello {}", field_name);
-
-
+ 
 }
 //------------------------------------------------------------------------------------------------------------
 
@@ -34,7 +31,7 @@ template<typename type_name> void TExample_Reflection(const type_name &object, i
 // AExample_Reflection
 void AExample_Reflection::Init()
 {
-    SObject object;
-    // TExample_Reflection(object, 5);
+    AObject object;
+    TExample_Reflection(object, 5);
 }
 //------------------------------------------------------------------------------------------------------------
