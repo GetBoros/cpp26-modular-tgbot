b9dd438bc526a068c1c2fd0b7723b86916ca355f
GetBoros <getboros@gmail.com>
2026-07-06 22:20:31 +0300

    doc: example func reflections

--- Lib_Module/src/Example_Reflection.cpp
+++ Lib_Module/src/Example_Reflection.cpp
@@ -11,6 +11,8 @@ module Example_Reflection;
 //------------------------------------------------------------------------------------------------------------
 class AObject
 {
+    void Temp();
+
     int Value_00;
     int Value_01;
 };
@@ -20,7 +22,28 @@ template<typename type_name> void TExample_Reflection(const type_name &object, i
     constexpr std::meta::info object_info = ^^std::remove_cvref_t<type_name>;
     constexpr std::meta::access_context access_ctx = std::meta::access_context::unchecked();
 
-    static constexpr auto fields = std::define_static_array(std::meta::nonstatic_data_members_of(object_info, access_ctx) );
+    static constexpr auto fields_data = std::define_static_array(std::meta::nonstatic_data_members_of(object_info, access_ctx) );
+    static constexpr auto fields_func = std::define_static_array(std::meta::members_of(object_info, access_ctx) );
+
+    template for(constexpr auto field_data : fields_data)
+    {
+        constexpr auto field_name = std::meta::identifier_of(field_data);
+
+        std::println("Field name: {}. ", field_name, object.[:field_data:]);
+    }
+
+    template for(constexpr auto field_func : fields_func)
+    {
+        constexpr auto is_identifier = std::meta::has_identifier(field_func);
+
+        if constexpr (is_identifier)
+        {
+            constexpr auto field_func_name = std::meta::identifier_of(field_func);  // Caching member name
+
+            std::println("Field func name: {}. ", field_func_name);
+
+        }
+    }
  
 }
 //------------------------------------------------------------------------------------------------------------
