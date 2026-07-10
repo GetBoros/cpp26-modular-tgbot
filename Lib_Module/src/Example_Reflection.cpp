bd6c069839673583c77ee510c2d79e2c52d4903d
GetBoros <getboros@gmail.com>
2026-07-10 22:17:03 +0300

    doc: temp

--- Lib_Module/src/Example_Reflection.cpp
+++ Lib_Module/src/Example_Reflection.cpp
@@ -47,7 +47,6 @@ template<typename type_name> void TExample_Reflection(const type_name &object, i
     // Add example AoS and Soa convert AoS to SoA 
     // Generate cpp type from json
     // Named tuple - create struct with named fields in compile time
-    // Cheat
 }
 //------------------------------------------------------------------------------------------------------------
 
