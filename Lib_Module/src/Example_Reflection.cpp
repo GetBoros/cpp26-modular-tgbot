0a1ce48403e760e3547fe27e8899be6601ba4b4f
GetBoros <getboros@gmail.com>
2026-07-12 22:19:03 +0300

    doc: Add final 7/7

--- Lib_Module/src/Example_Reflection.cpp
+++ Lib_Module/src/Example_Reflection.cpp
@@ -44,8 +44,8 @@ template<typename type_name> void TExample_Reflection(const type_name &object, i
 
         std::println("Field func name: {}. ", field_func_name);
     }
-    // Add example AoS and Soa convert AoS to SoA 
-    // Generate cpp type from json
+
+    // Add example AoS and Soa convert AoS to SoA || Generate cpp type from json 
     // Named tuple - create struct with named fields in compile time
 }
 //------------------------------------------------------------------------------------------------------------
