937e939ab80f3c993bd7e3fa360738cf7eca063d
GetBoros <getboros@gmail.com>
2026-07-25 22:25:55 +0300

    refact: Add test 9/14

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -13,11 +13,12 @@ import Lib_Clicker;
 //------------------------------------------------------------------------------------------------------------
 void Temp()
 {
-    constexpr AExample_Constexpr example_constexpr(10);  // Help
+    constexpr int value = 10;
+    constexpr AExample_Constexpr example_constexpr(value);  // Help
     
-    AExample_Constexpr example_constexpr_runtime(10);
+    AExample_Constexpr example_constexpr_runtime(value);
 
-    static_assert(example_constexpr.Constexpr_Value == 10);  // check
+    static_assert(example_constexpr.Constexpr_Value == value);  // check
     static_assert(example_constexpr.Example_Constexpr_Info() == 20);  // check second
 
     if (example_constexpr_runtime.Example_Constexpr_Info() == 20)  // result
