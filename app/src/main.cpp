4aab9f1e030b5edeb3eac24343c6a6e84efa6703
GetBoros <getboros@gmail.com>
2026-07-26 15:31:40 +0300

    refact: Add test 10/14

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -16,7 +16,7 @@ void Temp()
     constexpr int value = 10;
     constexpr AExample_Constexpr example_constexpr(value);  // Help
     
-    AExample_Constexpr example_constexpr_runtime(value);
+    AExample_Constexpr example_constexpr_runtime(value);  // temp
 
     static_assert(example_constexpr.Constexpr_Value == value);  // check
     static_assert(example_constexpr.Example_Constexpr_Info() == 20);  // check second
